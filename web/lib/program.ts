ec91ee7a6288d6fcef498b97e359fe486815493d
johngreetme <johngreetme@gmail.com>
2026-02-06 02:10:48 +0000

    feat: Define Solana program ID and IDL, and add UI utility dependencies.

---
+++ web/lib/program.ts
@@ -0,0 +1,5 @@
+import { PublicKey } from "@solana/web3.js";
+import idl from "./idl.json";
+
+export const PROGRAM_ID = new PublicKey("97aMxMjX3ANbg8mm1gVJCppUqZmo5oCmHuggASZ7Yup1");
+export const IDL = idl;
